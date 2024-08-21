f72c5b54695566c64b1ab4c3ba5dc84e5be14810
MUHAMAD CASDI <KNO2104136@smsfinance.co.id>
2024-08-21 11:35:11 +0700

    Install tailwind

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export interface ButtonProps {
     label: string;
+    className: string;
+    children: JSX.Element;
+    onClick: () => void
 }
 
 const Button = (props: ButtonProps) => {
-    return <button>{props.label}</button>;
+    return <button {...props}>{props.label}</button>;
 };
 
 export default Button;
