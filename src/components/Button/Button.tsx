58fa45e4b17ca4d94272245981bca61564962328
MUHAMAD CASDI <KNO2104136@smsfinance.co.id>
2024-08-22 15:54:48 +0700

    Created variant color button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { buttonColors } from "../../styles/colors/Colors";
 
-export interface ButtonProps {
-    label: string;
-    className: string;
-    children: JSX.Element;
-    onClick: () => void
+type Variant = 'primary' | 'secondary' | 'success' | 'warning' | 'error';
+
+interface ButtonProps {
+    variant?: Variant;
+    children: React.ReactNode;
 }
 
-const Button = (props: ButtonProps) => {
-    return <button {...props}>{props.label}</button>;
+const Button: React.FC<ButtonProps> = ({ variant = 'primary', children }) => {
+    // Map variant to Tailwind CSS classes
+    const variantClasses: Record<Variant, string> = buttonColors;
+
+    return (
+        <button className={`py-2 px-4 rounded ${variantClasses[variant]}`}> {children} </button>
+    );
 };
 
 export default Button;
---
+++ src/styles/colors/Colors.tsx
@@ -0,0 +1,7 @@
+export const buttonColors = {
+    primary: "border-blue-500 bg-blue-500 text-white",
+    secondary: "border-gray-900 bg-gray-900 text-white",
+    success: "border-green-500 bg-green-500 text-white",
+    warning: "border-yellow-400 bg-yellow-400 text-white",
+    error: "border-red-500 bg-red-500 text-white"
+}
