db4e1a610f0b352665762135f9f985aa9ae8b2ce
MUHAMAD CASDI <KNO2104136@smsfinance.co.id>
2024-08-22 16:49:14 +0700

    Fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./components/**/*.js"],
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
