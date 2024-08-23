60f1c4ecd38e983efea898798fa2fead8bd6bae0
MUHAMAD CASDI <KNO2104136@smsfinance.co.id>
2024-08-23 15:25:53 +0700

    Fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,38 +1,55 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import path from 'path';
+import terser from '@rollup/plugin-terser';
 import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 
 export default [
     {
-        input: "src/index.ts",
+        input: 'src/index.ts',
         output: [
             {
-                file: "dist/cjs/index.js",
-                format: "cjs",
+                file: path.resolve('dist/cjs/index.js'),
+                format: 'cjs',
                 sourcemap: true,
             },
             {
-                file: "dist/esm/index.js",
-                format: "esm",
+                file: path.resolve('dist/esm/index.js'),
+                format: 'esm',
                 sourcemap: true,
             },
         ],
         plugins: [
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
+            peerDepsExternal(),
             postcss({
-                extensions: ['.css'],
+                plugins: [tailwindcss, autoprefixer],
+                extract: true,
                 minimize: true,
                 sourceMap: true,
-                modules: true,
+                inject: true,
             }),
+            resolve(),
+            commonjs(),
+            typescript({
+                tsconfig: './tsconfig.json',
+            }),
+            babel({
+                babelHelpers: 'bundled',
+                exclude: 'node_modules/**',
+            }),
+            terser(), // Minifies the bundle
         ],
+        external: ['react', 'react-dom'],
+
     },
     {
-        input: "dist/esm/types/index.d.ts",
+        input: "dist/esm/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
         external: [/\.css$/],
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,27 @@
+import "../../styles/tailwind.css"
 import React from "react";
 import { buttonColors } from "../../styles/colors/Colors";
 
-type Variant = 'primary' | 'secondary' | 'success' | 'warning' | 'error';
+type Color = 'primary' | 'secondary' | 'success' | 'warning' | 'error';
 
-interface ButtonProps {
-    variant?: Variant;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    color?: Color;
     children: React.ReactNode;
+    className: string
+    onClik: () => void
 }
 
-const Button: React.FC<ButtonProps> = ({ variant = 'primary', children }) => {
+const Button: React.FC<ButtonProps> = ({ color = 'primary', children, className, ...props }) => {
     // Map variant to Tailwind CSS classes
-    const variantClasses: Record<Variant, string> = buttonColors;
+    const colorClasses: Record<Color, string> = buttonColors;
 
     return (
-        <button className={`py-2 px-4 rounded ${variantClasses[variant]}`}> {children} </button>
+        <button
+            className={`py-2 px-4 rounded ${color ? colorClasses[color] : colorClasses.primary, className}`}
+            {...props}
+        >
+            {children}
+        </button>
     );
 };
 
